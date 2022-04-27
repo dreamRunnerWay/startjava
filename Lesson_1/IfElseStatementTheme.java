291861cca93b501afcd638bd5e80b21dd672cfff
Alexander_Ivanitskii <asashakoval@yandex.ru>
2022-04-27 16:38:57 +0300

    implement HW_1_edit2

---
+++ Lesson_1/IfElseStatementTheme.java
@@ -0,0 +1,12 @@
+public class IfElseStatementTheme {
+    public static void main(String[] args) {
+        //Перевод псевдокода на язык Java
+        System.out.prontln("\n1.Перевод псевдокода на язык Java:");
+        if (age > 20) {
+            System.out.println("Сообщение");
+        } else {
+            System.out.println("Сообщение");
+        }
+
+        if ()
+    }
--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -21,19 +21,17 @@ public class VariablesTheme {
         System.out.println(result);
 
         //Расчёт стоимости товара со скидкой
-
         System.out.println("\n2.Расчёт стоимости товара со скидкой:");
         int productX = 100;
         int productY = 200;
         int saleValue = 11;
-        int fullProcent = 100;
-        int productSum = (productX + productY) * saleValue / fullProcent;
+        int fullPercent = 100;
+        int productSum = (productX + productY) * saleValue / fullPercent;
 
         System.out.println("Сумма скидки:" + " " + productSum + " " + "рубля");
         System.out.println("Цена с учётом скидки:" + " " + (productX + productY - productSum) + " " + "рублей");
 
         //Вывод на консоль слова JAVA
-
         System.out.println("\n3.Вывод на консоль слова JAVA:");
         System.out.println("   J    a  v     v  a   ");
         System.out.println("   J   a a  v   v  a a  ");
@@ -41,7 +39,6 @@ public class VariablesTheme {
         System.out.println(" JJ  a     a  V  a     a");
 
         //Отображение min и max значений числовых типов данных 
-
         System.out.println("\n4.Отображение min и max значений числовых типов данных:");
         byte byteDigit = 127;
         short shortDigit = 32767;
@@ -54,7 +51,6 @@ public class VariablesTheme {
         System.out.println(++longDigit + " " + --longDigit);
 
         //Перестановка значений переменных
-
         System.out.println("\n5.Перестановка значений переменных:");
         double num1 = 57.19d;
         double num2 = 141.57;
@@ -66,7 +62,6 @@ public class VariablesTheme {
         System.out.println(num2);
 
         //Вывод символов и их кодов
-
         System.out.println("\n6.Вывод символов и их кодов:");
         byte codeChar1 = 35;
         byte codeChar2 = 38;
@@ -74,14 +69,13 @@ public class VariablesTheme {
         byte codeChar4 = 94;
         byte codeChar5 = 95;
 
-        System.out.println((char) + 35);
-        System.out.println((char) + 38);
-        System.out.println((char) + 64);
-        System.out.println((char) + 94);
-        System.out.println((char) + 95);
+        System.out.println("35" + (char) + codeChar1);
+        System.out.println("38" + (char) + codeChar2);
+        System.out.println("64" + (char) + codeChar3);
+        System.out.println("94" + (char) + codeChar4);
+        System.out.println("95" + (char) + codeChar5);
 
         //Произведение и сумма цифр числа
-
         System.out.println("\n7.Произведение и сумма цифр числа:");
         int srcNum = 345;
         int digit1 = srcNum % 10; 
@@ -94,35 +88,32 @@ public class VariablesTheme {
         System.out.println("Сумма цифр числа:" + " " + sumDigits);
 
         //Вывод на консоль ASCII арт Дюка
-
         System.out.println("\n8.Вывод на консоль арт Дюка:");
         String slash1 = "/";
         String slash2 = "\\";
-        String breket1 = "(";
-        String breket2 = ")";
+        String lBreket = "(";
+        String rBreket = ")";
         String space = " ";
         String underscore = "_";
 
         System.out.println(space + space + space + space + slash1 + slash2 + space + space + space + space);
         System.out.println(space + space + space + slash1 + space + space + slash2);
-        System.out.println(space + space + slash1 + underscore + breket1 + space + breket2 + slash2);
+        System.out.println(space + space + slash1 + underscore + lBreket + space + rBreket + slash2);
         System.out.println(space + slash1 + space + space + space + space + space +  space + slash2);
         System.out.println(slash1 + underscore + underscore + underscore + underscore + slash1 + slash2 + underscore + underscore + slash2);
 
         //Отображение количества сотен, десятков и единиц числа 
-
         System.out.println("\n9.Отображение количества сотен, десятков и единиц числа: 123");
-        int scrDigit = 123; 
-        int digitSotnya = scrDigit / 100;
-        int digitDesyatka = scrDigit / 10 % 10;
-        int digitEdinica = scrDigit % 10;
+        int scrNum = 123; 
+        int digitHundred = scrNum / 100;
+        int digitTenth = scrNum / 10 % 10;
+        int digitSingle = scrNum % 10;
 
-        System.out.println(digitSotnya + " " + "Сотня");
-        System.out.println(digitDesyatka + " " + "Десятка");
-        System.out.println(digitEdinica + " " + "Единицы");
+        System.out.println(digitHundred + " " + "Сотня");
+        System.out.println(digitTenth + " " + "Десятка");
+        System.out.println(digitSingle + " " + "Единицы");
 
         //Преобразование секунд
-
         System.out.println("\n10.Преобразование секунд:");
         int seconds = 86399;
         int minutes = seconds / 60;
@@ -130,8 +121,8 @@ public class VariablesTheme {
         int minutes2 = minutes % 60;
         int seconds2 = seconds % 60;
 
-        System.out.println("Время:" + " " + hours+ ":" + minutes2 + ":" + seconds2);
-        }
+        System.out.println("Время:" + hours + ":" + minutes2 + ":" + seconds2);
+    }
 }
 
 
